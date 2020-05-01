bd34db2c0107fd6aa6b3d2f6e3300bd4c2960d05
CansuEraslan <eraslan.cansu.8@gmail.com>
2020-05-01 22:22:27 +0300

    12.Video hakkımızda,referanslar,iletişim

--- home/views.py
+++ home/views.py
@@ -2,10 +2,25 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from book.models import Category
 from home.models import Setting
 
 
 def index(request):
     setting=Setting.objects.get(pk=1)
-    context={'setting':setting}
+    context={'setting':setting,'page':'home'}
     return render(request, 'index.html', context)
+def hakkimizda(request):
+    setting=Setting.objects.get(pk=1)
+    category = Category.objects.all()
+    context={'setting':setting,'page':'hakkimizda','category':category}
+    return render(request, 'hakkimizda.html', context)
+def referanslar(request):
+    setting=Setting.objects.get(pk=1)
+    category = Category.objects.all()
+    context={'setting':setting,'category':category}
+    return render(request, 'referanslar.html', context)
+def iletisim(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting}
+    return render(request, 'iletisim.html', context)
--- project2/urls.py
+++ project2/urls.py
@@ -18,10 +18,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 
+from home import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('home/', include('home.urls')),
+    path('hakkimizda/',views.hakkimizda, name='hakkimizda'),
+    path('referanslar/',views.referanslar, name='referanslar'),
+    path('iletisim/',views.iletisim, name='iletisim'),
     path('book/', include('book.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
