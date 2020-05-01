59cc027a9b9983cb2484f787457d4baa45f563d6
CansuEraslan <eraslan.cansu.8@gmail.com>
2020-05-02 01:21:50 +0300

    13.Video iletişim formu

--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 from book.models import Category
-from home.models import Setting
+from home.models import Setting, ContactFormu, ContactFormMessage
 
 
 def index(request):
@@ -21,6 +22,19 @@ def referanslar(request):
     context={'setting':setting,'category':category}
     return render(request, 'referanslar.html', context)
 def iletisim(request):
-    setting = Setting.objects.get(pk=1)
-    context = {'setting': setting}
+    if request.method=='POST':
+        form=ContactFormu(request.POST)
+        if form.is_valid():
+            data=ContactFormMessage()
+            data.name=form.cleaned_data['name']
+            data.email=form.cleaned_data['email']
+            data.subject=form.cleaned_data['subject']
+            data.message=form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request,"Mesajınız Gönderilmiştir")
+            return  HttpResponseRedirect('/iletisim')
+    setting=Setting.objects.get(pk=1)
+    form=ContactFormu()
+    context={'setting':setting,'form':form}
     return render(request, 'iletisim.html', context)
