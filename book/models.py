cd692cfce88fac2afff0fff53d00769dcbbc7c83
CansuEraslan <eraslan.cansu.8@gmail.com>
2020-05-01 20:42:56 +0300

    Proje ilk upload

---
+++ book/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+# Create your models here.
+from django.utils.safestring import mark_safe
+
+
+class Category(models.Model):
+    STATUS=(
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    title= models.CharField(max_length=30)
+    keywords=models.CharField(max_length=255)
+    description=models.CharField(max_length=255)
+    image=models.ImageField(blank=True,upload_to='images/')
+    status=models.CharField(max_length=10,choices=STATUS)
+    slug=models.SlugField()
+    parent=models.ForeignKey('self',blank=True,null=True,related_name='children',on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_decription = 'Image'
+
+class Book(models.Model):
+    STATUS=(
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    title= models.CharField(max_length=30)
+    keywords=models.CharField(max_length=255)
+    description=models.CharField(max_length=255)
+    image=models.ImageField(blank=True,upload_to='images/')
+    yazar=models.CharField(max_length=50)
+    basimyili=models.IntegerField()
+    baskisira=models.IntegerField()
+    yayinevi=models.CharField(max_length=50)
+    stok= models.IntegerField()
+    detail=models.TextField()
+    status=models.CharField(max_length=10,choices=STATUS)
+    slug=models.SlugField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_decription = 'Image'
+
+class Images(models.Model):
+    book=models.ForeignKey(Book,on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    image = models.ImageField(blank=True, upload_to='images/')
+    def __str__(self):
+        return self.title
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_decription = 'Image'
