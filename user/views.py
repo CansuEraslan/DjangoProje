651e5020a89bdb5a6472cc12875fe78ef87c760a
CansuEraslan <eraslan.cansu.8@gmail.com>
2020-05-11 04:31:02 +0300

    32-33 üye sipariş listesi ve detayı,yorumları listeleme silme

--- user/views.py
+++ user/views.py
@@ -1,15 +1,17 @@
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordChangeForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from book.models import Category
+from book.models import Category, Comment
 from home.models import Setting, UserProfile
+from order.models import Order, OrderProduct
 from user.forms import UserUpdateForm, ProfileUpdateForm
 
-
+@login_required(login_url='/login')
 def index(request):
     category = Category.objects.all()
     setting = Setting.objects.get(pk=1)
@@ -18,6 +20,7 @@ def index(request):
     context = {'category': category,'setting':setting,'profile':profile}
     return render(request,'user_profile.html',context)
 
+@login_required(login_url='/login')
 def user_update(request):
     if request.method == 'POST':
         user_form = UserUpdateForm(request.POST,instance=request.user)
@@ -29,16 +32,19 @@ def user_update(request):
             return redirect('/user')
     else:
         category = Category.objects.all()
+        setting = Setting.objects.get(pk=1)
         user_form = UserUpdateForm(instance=request.user)
         profile_form =ProfileUpdateForm(instance=request.user.userprofile)
         context = {
             'category': category,
+            'setting': setting,
             'user_form': user_form,
             'profile_form': profile_form,
 
         }
         return render(request,'user_update.html',context)
 
+@login_required(login_url='/login')
 def change_password(request):
     if request.method == 'POST':
         form = PasswordChangeForm(request.user,request.POST)
@@ -52,9 +58,60 @@ def change_password(request):
             return HttpResponseRedirect('/user/password')
     else:
         category =Category.objects.all()
+        setting = Setting.objects.get(pk=1)
         form = PasswordChangeForm(request.user)
         return render(request,'change_password.html',{
             'form':form,
-            'category': category
+            'category': category,
+            'setting': setting,
 
         })
+
+@login_required(login_url='/login')
+def orders(request):
+    category = Category.objects.all()
+    setting = Setting.objects.get(pk=1)
+    current_user=request.user
+    orders=Order.objects.filter(user_id=current_user.id)
+    context = {
+        'category': category,
+        'setting': setting,
+        'orders': orders,
+    }
+    return render(request,'user_orders.html',context)
+
+@login_required(login_url='/login')
+def orderdetail(request, id):
+    category = Category.objects.all()
+    setting = Setting.objects.get(pk=1)
+    current_user = request.user
+    order = Order.objects.get(user_id=current_user.id, id=id)
+    orderitems = OrderProduct.objects.filter(order_id=id)
+    context = {
+        'category': category,
+        'setting': setting,
+        'order': order,
+        'orderitems': orderitems,
+
+    }
+    return render(request, 'user_order_detail.html', context)
+
+@login_required(login_url='/login')
+def comments(request):
+    category = Category.objects.all()
+    setting = Setting.objects.get(pk=1)
+    current_user = request.user
+    comments = Comment.objects.filter(users_id=current_user.id)
+    context = {
+        'category': category,
+        'setting': setting,
+        'comments': comments,
+    }
+    return render(request, 'user_comments.html', context)
+
+@login_required(login_url='/login')
+def deletecomment(request,id):
+    current_user = request.user
+    Comment.objects.filter(id=id, users_id=current_user.id).delete()
+    messages.success(request, 'Yorum silindi')
+    return HttpResponseRedirect('/user/comments')
